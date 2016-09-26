36dfe64d2a7d692e3742c3c156d455ea83d1892d
Rene Schulte <contact@rene-schulte.info>
2016-09-26 10:24:41 +0200

    Initial checkin

---
+++ HoloFame/Assets/Scripts/PlacementBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class PlacementBehavior : MonoBehaviour
+{
+    private GestureRecognizer _gestureRecognizer;
+
+    public Camera Camera;
+    public bool IsPlaced;
+
+    void Start()
+    {
+        _gestureRecognizer = new GestureRecognizer();
+        _gestureRecognizer.TappedEvent += GestureRecognizerOnTappedEvent;
+        _gestureRecognizer.SetRecognizableGestures(GestureSettings.Tap);
+        _gestureRecognizer.StartCapturingGestures();
+    }
+
+    private void GestureRecognizerOnTappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
+    {
+        IsPlaced = !IsPlaced;
+    }
+
+    void Update()
+    {
+        if (IsPlaced) return;
+
+        var raycastHits = Physics.RaycastAll(Camera.transform.position, Camera.transform.forward);
+        var firstHit = raycastHits.OrderBy(r => r.distance).FirstOrDefault();
+
+        transform.position = firstHit.point;
+        transform.forward = firstHit.normal;
+    }
+
+    void Reset()
+    {
+        Camera = Camera.main;
+    }
+
+    private void OnDestroy()
+    {
+        if (_gestureRecognizer != null)
+        {
+            if (_gestureRecognizer.IsCapturingGestures())
+            {
+                _gestureRecognizer.StopCapturingGestures();
+            }
+            _gestureRecognizer.Dispose();
+        }
+    }
+}
---
+++ HoloFame/Assets/Scripts/SpeechHandler.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Windows.Speech;
+
+public class SpeechHandler : MonoBehaviour
+{
+    public string HidePlaneCmd = "hide plane";
+    public GameObject Plane;
+
+    public string ResetSceneCmd = "reset scene";
+
+    private KeywordRecognizer _keywordRecognizer;
+
+    void Start()
+    {
+        _keywordRecognizer = new KeywordRecognizer(new[] { HidePlaneCmd, ResetSceneCmd });
+        _keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        _keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        var cmd = args.text;
+        if (cmd == HidePlaneCmd)
+        {
+            Plane.SetActive(false);
+        }
+        else if (cmd == ResetSceneCmd)
+        {
+            SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (_keywordRecognizer != null)
+        {
+            if (_keywordRecognizer.IsRunning)
+            {
+                _keywordRecognizer.Stop();
+            }
+            _keywordRecognizer.Dispose();
+        }
+    }
+}
