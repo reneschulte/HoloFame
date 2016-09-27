c028ad28922d4ae3c42a754dee556b1b63e4e93a
Rene Schulte <contact@rene-schulte.info>
2016-09-27 17:23:10 +0200

    Added ConcreteBehavior for hand proximity interaction and tap+hold gesture. B
    Base demo done.

--- HoloFame/Assets/Scripts/PlacementBehavior.cs
+++ HoloFame/Assets/Scripts/PlacementBehavior.cs
@@ -1,20 +1,36 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.VR.WSA.Input;
 
 public class PlacementBehavior : MonoBehaviour
 {
     private GestureRecognizer _gestureRecognizer;
+    private Vector3 _previousOffset;
 
     public Camera Camera;
     public bool IsPlaced;
 
     void Start()
     {
+        // Attach gesture handlers
         _gestureRecognizer = new GestureRecognizer();
         _gestureRecognizer.TappedEvent += GestureRecognizerOnTappedEvent;
-        _gestureRecognizer.SetRecognizableGestures(GestureSettings.Tap);
+        _gestureRecognizer.NavigationStartedEvent += GestureRecognizerOnNavigationStartedEvent;
+        _gestureRecognizer.NavigationUpdatedEvent += GestureRecognizerOnNavigationUpdatedEvent;
+        _gestureRecognizer.SetRecognizableGestures(GestureSettings.Tap | GestureSettings.NavigationY);
         _gestureRecognizer.StartCapturingGestures();
+    }
+
+    private void GestureRecognizerOnNavigationStartedEvent(InteractionSourceKind source, Vector3 normalizedOffset, Ray headRay)
+    {
+        _previousOffset = normalizedOffset;
+    }
+
+    private void GestureRecognizerOnNavigationUpdatedEvent(InteractionSourceKind source, Vector3 normalizedOffset, Ray headRay)
+    {
+        transform.localPosition += (normalizedOffset - _previousOffset);
+        _previousOffset = normalizedOffset;
     }
 
     private void GestureRecognizerOnTappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
@@ -27,10 +43,10 @@ public class PlacementBehavior : MonoBehaviour
         if (IsPlaced) return;
 
         var raycastHits = Physics.RaycastAll(Camera.transform.position, Camera.transform.forward);
-        var firstHit = raycastHits.OrderBy(r => r.distance).FirstOrDefault();
+        var firstHit = raycastHits.Where(r => r.transform != transform).OrderBy(r => r.distance).FirstOrDefault();
 
         transform.position = firstHit.point;
-        transform.forward = firstHit.normal;
+        transform.up = firstHit.normal;
     }
 
     void Reset()
